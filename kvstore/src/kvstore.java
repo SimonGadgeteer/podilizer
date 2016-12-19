68c906451ef45197a29b8e6c9514f2b67cf3eb2d
Martin <martin@kaktuspalme.ch>
2016-12-19 11:31:27 +0100

    Basic kvstore

--- kvstore/src/kvstore.java
+++ kvstore/src/kvstore.java
@@ -1,5 +1,25 @@
-public class kvstore {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class kvstore {
+    public HashMap<String, String> keyvalues = new HashMap();
+
+    public static void main(String[] args) {
+        kvstore store = new kvstore();
+
+        if(args.length == 1) {
+            System.out.println(store.read(args[0]));
+        } else if(args.length == 2) {
+            store.write(args[0], args[1]);
+        } else {
+            System.out.println("Invalid arguments provided");
+        }
+    }
+
+    private String read(String key) {
+        return keyvalues.get(key);
+    }
+
+    private void write(String key, String value) {
+        keyvalues.put(key, value);
     }
 }
